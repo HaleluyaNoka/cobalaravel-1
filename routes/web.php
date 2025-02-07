acaf66d395d9dfe6e71fffa37912fe2289edddfe
Haleluya Noka <89540592+HaleluyaNoka@users.noreply.github.com>
2025-02-08 00:34:02 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,11 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 Route::get('/about', function () {
     return view('about', ['nama' => 'Noka', 'title' => 'About']);
 });
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
